bdd6b7f0853e0ffdb1570d41ba1022c7ee920ecc
tedhull <jedi.f.sh@gmail.com>
2025-07-16 14:29:43 +0400

    odoo

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import {useState} from "react";
 
 export default function Navbar({
                                    save,
@@ -19,111 +20,137 @@ export default function Navbar({
                                    restrictSelected,
                                    unbanSelected,
                                }) {
+    const [showAlert, setShowAlert] = useState(false);
+    const [alertMessage, setAlertMessage] = useState('');
+    const handleClick = async () => {
+        console.log("clicked");
+        try {
+            await navigator.clipboard.writeText(localStorage.getItem('access_token'));
+            setShowAlert(true);
+            setAlertMessage('token was copied to clipboard');
+        } catch (err) {
+            console.error('Failed to copy:', err);
+        }
+        setTimeout(() => {
+            setShowAlert(false);
+            setAlertMessage('');
+        }, 3000);
+    }
     return (
-
-        <nav className="navbar redactor-navbar fixed-top navbar-border navbar-expand-lg px-3">
-            <a className="navbar-brand" href="/">
-                <img src="../docs/4.0/assets/brand/bootstrap-solid.svg" width="30" height="30"
-                     className="d-inline-block align-top" alt=""/>
-                <span className={"brand-text ms-2"}>Myforms</span>
-            </a>
-            <div className="d-flex position-relative" role="search">
-                <input className="form-control search-line me-2" type="search" placeholder="Find Template"
-                       aria-label="Search"/>
-                <button className="btn btn-primary">Search</button>
-            </div>
-
-            {view !== "form" && editing && userStatus !== "viewer" && (
-                <div className="navbar-center-tabs-container">
-                    <ul className="nav nav-underline navbar-center-tabs justify-content-center">
-                        <li className="nav-item">
-                            <a className={`nav-link ${view === "redactor" ? "active" : ""}`}
-                               href={`/edit/${templateId}`}>Redactor</a>
-                        </li>
-                        <li className="nav-item">
-                            <a className={`nav-link ${view === "table" ? "active" : ""}`}
-                               href={`/list/${templateId}`}>Responses</a>
-                        </li>
-                    </ul>
+        <div>
+            <nav className="navbar redactor-navbar fixed-top navbar-border navbar-expand-lg px-3">
+                <a className="navbar-brand" href="/">
+                    <img src="../../public/logo192.png" width="30" height="30"
+                         className="d-inline-block align-top" alt=""/>
+                    <span className={"brand-text ms-2"}>Myforms</span>
+                </a>
+                <div className="d-flex position-relative" role="search">
+                    <input className="form-control search-line me-2" type="search" placeholder="Find Template"
+                           aria-label="Search"/>
+                    <button className="btn btn-primary">Search</button>
                 </div>
-            )}
-            {view === "account" && (
-                <div className="navbar-center-tabs-container">
-                    <ul className="nav nav-underline navbar-center-tabs justify-content-center">
-                        <li className="nav-item">
-                            <a className={`nav-link ${accountSection === "forms" ? "active" : ""}`}
-                               href={`/user/forms`}>forms</a>
-                        </li>
-                        <li className="nav-item">
-                            <a className={`nav-link ${accountSection === "templates" ? "active" : ""}`}
-                               href={`/user/templates`}>templates</a>
-                        </li>
-                        {userStatus === "admin" && (
+                {view !== "form" && editing && userStatus !== "viewer" && (
+                    <div className="navbar-center-tabs-container">
+                        <ul className="nav nav-underline navbar-center-tabs justify-content-center">
                             <li className="nav-item">
-                                <a className={`nav-link ${accountSection === "users" ? "active" : ""}`}
-                                   href={`/user/admin`}>users</a>
+                                <a className={`nav-link ${view === "redactor" ? "active" : ""}`}
+                                   href={`/edit/${templateId}`}>Redactor</a>
                             </li>
-                        )}
-                    </ul>
-                </div>
-            )}
-
-            <div className="ms-auto d-flex gap-2">
-                {view === "redactor" && (
-                    <div className={"ms-auto d-flex gap-2"}>
-                        {userStatus !== "viewer" && !editing &&
-                            (<button className="btn btn-primary" onClick={save}>Save</button>)}
-                        {userStatus !== "viewer" && editing &&
-                            (<button className={"btn btn-primary"} onClick={edit}>Save</button>)}
-                        {userStatus === "creator" && (
-                            <button className="btn btn-primary" onClick={publish}>Publish</button>
-                        )}
-                        {userStatus !== "creator" && (
-                            <button className="btn btn-primary" onClick={save}>Add To My Templates </button>
-                        )}
+                            <li className="nav-item">
+                                <a className={`nav-link ${view === "table" ? "active" : ""}`}
+                                   href={`/list/${templateId}`}>Responses</a>
+                            </li>
+                        </ul>
                     </div>
                 )}
-                {view === "form" && (
-                    <div className={"ms-auto d-flex gap-2"}>
-                        <button className="btn btn-primary" onClick={submit}>Submit</button>
-                    </div>
-                )}
-                {view === "account" && hasSelectedRows && (
-                    <div className={"ms-auto d-flex gap-2"}>
-                        <button className="btn btn-danger" onClick={deleteSelected}>Delete</button>
-                        {accountSection === "users" && (
-                            <div className={"ms-auto d-flex gap-2"}>
-                                <button className={"btn btn-danger ms-auto"} onClick={banSelected}>Ban</button>
-                                <button className={"btn btn-danger ms-auto"} onClick={restrictSelected}>Restrict
-                                </button>
-                                <button className="btn btn-primary ms-auto" onClick={unbanSelected}>Unban</button>
-                                <button className="btn btn-success ms-auto" onClick={promoteSelected}>Grant</button>
-                            </div>
-                        )}
+                {view === "account" && (
+                    <div className="navbar-center-tabs-container">
+                        <ul className="nav nav-underline navbar-center-tabs justify-content-center">
+                            <li className="nav-item">
+                                <a className={`nav-link ${accountSection === "forms" ? "active" : ""}`}
+                                   href={`/user/forms`}>forms</a>
+                            </li>
+                            <li className="nav-item">
+                                <a className={`nav-link ${accountSection === "templates" ? "active" : ""}`}
+                                   href={`/user/templates`}>templates</a>
+                            </li>
+                            {userStatus === "admin" && (
+                                <li className="nav-item">
+                                    <a className={`nav-link ${accountSection === "users" ? "active" : ""}`}
+                                       href={`/user/admin`}>users</a>
+                                </li>
+                            )}
+                        </ul>
                     </div>
                 )}
 
-                <div className="dropdown">
-                    <button
-                        className="btn btn-secondary dropdown-toggle"
-                        type="button"
-                        id="dropdownMenuButton"
-                        data-bs-toggle="dropdown"
-                        aria-expanded="false"
-                    >
-                        {username}
-                    </button>
-                    <ul className="dropdown-menu" aria-labelledby="dropdownMenuButton">
-                        <li><a className="dropdown-item" href="/user/forms">Account</a></li>
-                        <li><a className="dropdown-item" href="/logout">Log Out</a></li>
-                        <li><a className={"dropdown-item"} href={`/connect/${process.env.REACT_APP_SF_FORM_ID}`}>Add Salesforce</a>
-                        </li>
-                        <button className="btn"
-                                onClick={toggleTheme}>{localStorage.getItem('theme') === 'light' ? "Light Mode" : "Dark Mode"}</button>
-                    </ul>
+                <div className="ms-auto d-flex gap-2">
+                    {view === "redactor" && (
+                        <div className={"ms-auto d-flex gap-2"}>
+                            {userStatus !== "viewer" && !editing &&
+                                (<button className="btn btn-primary" onClick={save}>Save</button>)}
+                            {userStatus !== "viewer" && editing &&
+                                (<button className={"btn btn-primary"} onClick={edit}>Save</button>)}
+                            {userStatus === "creator" && (
+                                <button className="btn btn-primary" onClick={publish}>Publish</button>
+                            )}
+                            {userStatus !== "creator" && (
+                                <button className="btn btn-primary" onClick={save}>Add To My Templates </button>
+                            )}
+                        </div>
+                    )}
+                    {view === "form" && (
+                        <div className={"ms-auto d-flex gap-2"}>
+                            <button className="btn btn-primary" onClick={submit}>Submit</button>
+                        </div>
+                    )}
+                    {view === "account" && hasSelectedRows && (
+                        <div className={"ms-auto d-flex gap-2"}>
+                            <button className="btn btn-danger" onClick={deleteSelected}>Delete</button>
+                            {accountSection === "users" && (
+                                <div className={"ms-auto d-flex gap-2"}>
+                                    <button className={"btn btn-danger ms-auto"} onClick={banSelected}>Ban</button>
+                                    <button className={"btn btn-danger ms-auto"} onClick={restrictSelected}>Restrict
+                                    </button>
+                                    <button className="btn btn-primary ms-auto" onClick={unbanSelected}>Unban</button>
+                                    <button className="btn btn-success ms-auto" onClick={promoteSelected}>Grant</button>
+                                </div>
+                            )}
+                        </div>
+                    )}
+                    <div className="dropdown">
+                        <button
+                            className="btn btn-secondary dropdown-toggle"
+                            type="button"
+                            id="dropdownMenuButton"
+                            data-bs-toggle="dropdown"
+                            aria-expanded="false"
+                        >
+                            {username}
+                        </button>
+                        <ul className="dropdown-menu" aria-labelledby="dropdownMenuButton">
+                            <li><a className="dropdown-item" href="/user/forms">Account</a></li>
+                            <li><a className="dropdown-item" href="/logout">Log Out</a></li>
+                            <li><a className={"dropdown-item"} href={`/connect/${process.env.REACT_APP_SF_FORM_ID}`}>Add
+                                Salesforce</a>
+                            </li>
+                            <button className={"btn ms-1"}
+                                    onClick={handleClick}>Get Token
+                            </button>
+                            <button className="btn ms-1"
+                                    onClick={toggleTheme}>{localStorage.getItem('theme') === 'light' ? "Light Mode" : "Dark Mode"}</button>
+                        </ul>
+
+                    </div>
                 </div>
+            </nav>
+            <div className="container">
+                {showAlert && (
+                    <div className="alert alert-success mt-lg-5" role="alert">
+                        {alertMessage}
+                    </div>
+                )}
             </div>
-        </nav>
-    )
-        ;
+        </div>
+    );
 }
